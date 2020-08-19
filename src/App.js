7339ec6c7404c74615203932dfab93c44c9c6c1c
sgdg <seungdeng17@gmail.com>
2020-08-19 20:14:56 +0900

    feat: 클라이언트 초기 세팅

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Join from './components/Join';
+import Chat from './components/Chat';
+
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/' component={Join} />
+                <Route exact path='/chat' component={Chat} />
+            </Switch>
+        </Router>
+    )
+}
+
+export default App;
