a5fc5e33bf8dd0b6628520c286189f50668312d5
sgdg <seungdeng17@gmail.com>
2020-08-19 21:23:32 +0900

    feat: 유저 데이터 추가, 제거 모듈 추가

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -11,7 +13,13 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection!!!');
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if (error) return callback(error);
+
+        socket.join(user.room);
+    });
 
     socket.on('disconnect', () => {
         console.log('User has left!!!');
